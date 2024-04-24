7b1eacd23236781cfc65220c6e3de99c021af9d0
42501w <syedaryana869@gmail.com>
2024-04-24 12:43:47 +0500

    This is CLI based currency exchange app and i added decimal restriction too in this app

---
+++ main.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1.0, // Base Currency
+  EUR: 0.83,
+  GBP: 0.72,
+  JPY: 109.77,
+  AUD: 1.29,
+  CAD: 1.25,
+  PKR: 278,
+};
+
+let user_answer: any = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter Your Amount",
+    type: "number",
+  },
+]);
+
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let Amount = user_answer.amount;
+let baseAmount = Amount / fromAmount; // USD base currency // 4
+let convertedAmount = Math.round(baseAmount * toAmount); // Round the result to the nearest integer
+console.log(convertedAmount);
